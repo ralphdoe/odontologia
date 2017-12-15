26e60034485b47d7fcf28510af0c2542dc976b6e
ralphdoe <ralphode12@gmail.com>
2017-12-15 18:38:07 -0500

    Cost center Service

--- costCenter/src/main/java/co/microservices/costCenter/controller/CostCenterAPIController.java
+++ costCenter/src/main/java/co/microservices/costCenter/controller/CostCenterAPIController.java
@@ -1,24 +1,26 @@
 package co.microservices.costCenter.controller;
 
-import co.microservices.costCenter.model.Appointment;
 import co.microservices.costCenter.model.CostCenter;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
- * Appointment API Controller
+ * CostCenter API Controller
  * Created by rafaellopez on 12/14/17.
  */
 @RestController
-public class AppointmentAPIController {
+public class CostCenterAPIController {
 
-    @RequestMapping(method = RequestMethod.GET, value = "/costcentergit")
-    public ResponseEntity<CostCenter> getCostCenter(@RequestBody final Appointment appointment) {
-
-        return new ResponseEntity<>(HttpStatus.OK);
+    @RequestMapping(method = RequestMethod.GET, value = "/costcenter")
+    public ResponseEntity<List<CostCenter>> getCostCenter() {
+        final List<CostCenter> costCenters = new ArrayList<>();
+        costCenters.add(new CostCenter("1", "Aguacatala"));
+        return new ResponseEntity<>(costCenters, HttpStatus.OK);
     }
 }
--- costCenter/src/main/java/co/microservices/costCenter/model/CostCenter.java
+++ costCenter/src/main/java/co/microservices/costCenter/model/CostCenter.java
@@ -4,4 +4,38 @@ package co.microservices.costCenter.model;
  * Created by rafaellopez on 12/15/17.
  */
 public class CostCenter {
+    private String id;
+    private String name;
+
+    public CostCenter() {
+    }
+
+    public CostCenter(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "CostCenter{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                '}';
+    }
 }
