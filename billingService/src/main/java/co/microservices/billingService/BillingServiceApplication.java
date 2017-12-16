9c52d9eef5e78652ff71e6f0f2c7a63c9874ca4b
ralphdoe <ralphode12@gmail.com>
2017-12-15 19:20:03 -0500

    Billing Consumer

--- billingService/src/main/java/co/microservices/billingService/BillingServiceApplication.java
+++ billingService/src/main/java/co/microservices/billingService/BillingServiceApplication.java
@@ -2,11 +2,13 @@ package co.microservices.billingService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class BillingServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BillingServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BillingServiceApplication.class, args);
+    }
 }
